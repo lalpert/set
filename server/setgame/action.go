2e25c923f55067f71dba557b1a728a2277bd32be
Russell Cohen <russell.r.cohen@gmail.com>
2016-03-23 23:04:06 -0700

    Random random

--- server/setgame/action.go
+++ server/setgame/action.go
@@ -2,8 +2,7 @@ package setgame
 
 // Action describes the action that lead to a boardstate
 type Action struct {
-    Type string `json:"type"`
-    PlayerID int32 `json:"playerId"`
-    CardIds []int `json:"cardIds"`
+	Type     string `json:"type"`
+	PlayerID int32  `json:"playerId"`
+	CardIds  []int  `json:"cardIds"`
 }
-
